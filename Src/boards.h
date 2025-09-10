93c38a9d924c0570f0f7a96a42edfca38c211e0f
Filo301 <filo301@gmail.com>
2025-09-10 03:55:09 +0200

    Update boards.h to fix LEDs

--- Src/boards.h
+++ Src/boards.h
@@ -4,10 +4,10 @@
 #if BOARD_ID==ENTREE
 #define IOPIN_LED0    GPIO_PIN_1
 #define IOPIN_LED1    GPIO_PIN_0
-#define IOPIN_PORT    GPIOB
+#define IOPIN_PORT    GPIOA
 // default
 #else
 #define IOPIN_LED0    GPIO_PIN_1
 #define IOPIN_LED1    GPIO_PIN_0
-#define IOPIN_PORT    GPIOB
+#define IOPIN_PORT    GPIOA
 #endif
